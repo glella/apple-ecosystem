18577c6eb16ada92235ee38f8a567300a7a22ef4
Guillermo Lella <arkorott@gmail.com>
2020-08-23 18:02:17 -0400

    Update book answer

--- Hacking iOS - UIKit/Chap 1 - Challenge Fizz Buzz Test.playground/Contents.swift
+++ Hacking iOS - UIKit/Chap 1 - Challenge Fizz Buzz Test.playground/Contents.swift
@@ -18,3 +18,24 @@ fizzbuzz(number: 3)     // Fizz
 fizzbuzz(number: 5)     // Buzz
 fizzbuzz(number: 15)    // Fizz Buzz
 fizzbuzz(number: 16)    // "16"
+
+// Proposed book solution
+// Better in terms of returning right away with response with no need to go through the rest of the statements
+// and no local variable
+
+func fizzbuzz2(number: Int) -> String {
+    
+    if number % 3 == 0 && number % 5 == 0 {
+        return "Fizz Buzz"
+    } else if number % 3 == 0 {
+        return "Fizz"
+    } else if number % 5 == 0 {
+        return "Buzz" } else {
+      return String(number)
+   }
+}
+
+fizzbuzz2(number: 3)     // Fizz
+fizzbuzz2(number: 5)     // Buzz
+fizzbuzz2(number: 15)    // Fizz Buzz
+fizzbuzz2(number: 16)    // "16"
