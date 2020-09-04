172f00ac5a871925a5baf21089c3693802a85a9c
Guillermo Lella <arkorott@gmail.com>
2020-09-04 17:32:09 -0400

    Project4

---
+++ Trying SwiftIU/LiquidSwipe/LiquidSwipe/LiquidSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidSwipeApp.swift
+//  LiquidSwipe
+//
+//  Created by Guillermo Lella on 9/2/20.
+//
+
+import SwiftUI
+
+@main
+struct LiquidSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
